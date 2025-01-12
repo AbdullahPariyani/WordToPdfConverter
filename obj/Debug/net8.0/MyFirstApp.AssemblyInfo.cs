24a54682c8d8b51d3077b9a5f8e853d05d017067
shehzad031248 <151955997+shehzad031248@users.noreply.github.com>
2025-01-12 09:15:17 +0300

    Chagnes

--- obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyFirstApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d26cf416fcba532e79c5e66c585eff05bff0ac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b091c4abcbc6300f9196262405f576bb5eaa6cb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
